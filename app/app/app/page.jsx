264518480a1807e1beae51f5b4acf1b2e755f4e9
zmiller2025 <zerotraceprocleaning@gmail.com>
2025-12-27 23:07:52 -0500

    Create page.jsx
    add home page

---
+++ app/app/app/page.jsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Zero Trace Pro Cleaning</h1>
+      <p>Commercial • Residential • New Builds</p>
+      <p>Serving the Indianapolis area</p>
+      <p>
+        📞 <a href="tel:13174897255">(317) 489-7255</a><br />
+        ✉️ <a href="mailto:zerotraceprocleaning@gmail.com">
+          zerotraceprocleaning@gmail.com
+        </a>
+      </p>
+    </main>
+  );
+}
